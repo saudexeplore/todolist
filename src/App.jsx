f8ff02da660bff326e6127d20adc626fb6eb7510
Saud Khan <saudexeplore@gmail.com>
2023-01-14 17:59:12 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -2,63 +2,63 @@ import React, { useState } from "react";
 import ToDoLists from "./ToDoLists";
 
 
-const App  = () =>{
+const App = () => {
 
     const [itemList, setItemList] = useState();
     const [Items, setItems] = useState([]);
 
 
-    const eventValue = (event) =>{
+    const eventValue = (event) => {
         setItemList(event.target.value);
     }
 
-   const  listOfItems = (e)=>{
-     setItems((oldItems) =>{
-        return [...oldItems, itemList]
-        
-        
-        
-        // if(itemList === ""){
-        //     console.log("done");
-        // }else{
-            
-        //     return [...oldItems, itemList]
-        // }
-        
-    })
-    setItemList("");
-
-    }
-    const deleteItem = (id) =>{
-       setItems((oldItems)=>{
-        return oldItems.filter((arrElement, index) =>{
-            return index !== id;
-        })
-       })
-    }
-
+    const listOfItems = (e) => {
+        // setItems((oldItems) => {
+        //             return [...oldItems, itemList]
     
- return(
-    <>
-        <div className="main-div">
-            <h1>ToDo List App</h1>
-            <div className="field-div">
-            <input type="text" value={itemList} placeholder="Add a Item..." onChange={eventValue} 
+        //         })
+        if (itemList === "") {
+            alert("please enter a valid Name")
+        } else {
+            setItems((oldItems) => {
+                return [...oldItems, itemList]
 
-            />
-            <button onClick={listOfItems}>+</button>
+            })
+        }
+
+        setItemList("");
+
+    }
+    const deleteItem = (id) => {
+        setItems((oldItems) => {
+            return oldItems.filter((arrElement, index) => {
+                return index !== id;
+            })
+        })
+    }
+
+
+    return (
+        <>
+            <div className="main-div">
+                <h1>ToDo List App</h1>
+                <div className="field-div">
+                    <input type="text" value={itemList} placeholder="Add a Item..." onChange={eventValue}
+
+                    />
+                    <button onClick={listOfItems}>+</button>
+                </div>
+
+                <ol>
+
+                    {Items.map((itemVal, index) => {
+                        return <ToDoLists text={itemVal} key={index} id={index} onSelect={deleteItem} />
+                    })}
+                </ol>
+
             </div>
-
-            <ol>
-                
-                {Items.map( (itemVal, index)=>{
-                    return <ToDoLists  text={itemVal} key={index} id={index} onSelect={deleteItem} />
-            })}
-            </ol>
-
-        </div>
-    </>
- )
+        </>
+    )
 }
 
 
